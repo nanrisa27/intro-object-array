43c5ca8b7ca92c3220ee7cddbfd718bfa6bcaf90
Minyoi87 <minyoi2710@gmail.com>
2020-01-20 13:55:54 +0200

    ja

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-window.dataLayer.array.forEach(function(event) {
+window.dataLayer.forEach(function(event) {
   var newEvent = document.createElement("li");
   newEvent.innerHTML =
     '<div class="info"><h2 class="title">' +
     event.short_description.fi +
     '</h2><p class= "desc">' +
-    event.dicription.description.fi +
+    event.description.fi +
     "</p></div>";
 
   document.getElementById("event-list").appendChild(newEvent);
